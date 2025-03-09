3b89c3caed5716bff6b13a5c6878438439ef8172
Brian Tootle <btootle@d3corp.com>
2025-03-09 15:45:52 -0400

    Added semantic structure and initial react components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <img src="/images/logo.jpg" alt="Little Lemon Logo" />
+    </header>
+  );
+}
+
+export default Header;
